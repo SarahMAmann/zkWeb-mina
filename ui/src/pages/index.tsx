da564839241ccbef7af2ba109bc0e425a3861590
Sarah Benson <sarahmbenson96@gmail.com>
2024-08-15 21:43:40 -0500

    frontend

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -6,6 +6,8 @@ import GradientBG from '../components/GradientBG.js';
 import styles from '../styles/Home.module.css';
 import heroMinaLogo from '../../public/assets/hero-mina-logo.svg';
 import arrowRightSmall from '../../public/assets/arrow-right-small.svg';
+import zkwebLogo from '../../public/assets/zkweb-logo.svg';
+import zkwebBannerImage from '../../public/assets/zkweb-banner-image.svg';
 
 export default function Home() {
   const [message, setMessage] = useState('');
@@ -46,122 +48,123 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Mina zkApp UI</title>
-        <meta name="description" content="built with o1js" />
-        <link rel="icon" href="/assets/favicon.ico" />
-      </Head>
-      <GradientBG>
-        <main className={styles.main}>
-          <div className={styles.center}>
-            <a
-              href="https://minaprotocol.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+    <Head>
+      <title>Mina zkApp UI</title>
+      <meta name="description" content="built with o1js" />
+      <link rel="icon" href="/assets/favicon.ico" />
+    </Head>
+    <GradientBG>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src={zkwebBannerImage}
+              alt="Mina Logo"
+              width={500}
+              height="400"
+              priority
+            />
+          </a>
+          {/* <p className={styles.tagline}>
+            built with
+            <code className={styles.code}>o1js</code>
+          </p> */}
+          <button className="custom-button" onClick={() => deployContract()}>Prove</button>
+
+        </div>
+        {/* <p className={styles.start}>
+          Get started by editing
+          <code className={styles.code}>src/pages/index.js</code> or <code className={styles.code}>src/pages/index.tsx</code>
+        </p> */}
+        <div className={styles.grid}>
+          <a
+            href=""
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              <span>EXAMPLE 1</span>
+              <div>
+                <Image
+                  src={zkwebLogo}
+                  alt="Mina Logo"
+                  width={36}
+                  height={36}
+                  priority
+                />
+              </div>
+            </h2>
+            <p>Correct key was provided</p>
+          </a>
+          <a
+            href=""
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              <span>EXAMPLE 2</span>
+              <div>
               <Image
-                className={styles.logo}
-                src={heroMinaLogo}
-                alt="Mina Logo"
-                width="191"
-                height="174"
-                priority
-              />
-            </a>
-            <p className={styles.tagline}>
-              built with
-              <code className={styles.code}>o1js</code>
-            </p>
-            <button className='cursor-pointer' onClick={() => deployContract()}>Deploy</button>
-          </div>
-          <p className={styles.start}>
-            Get started by editing
-            <code className={styles.code}>src/pages/index.js</code> or <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div className={styles.grid}>
-            <a
-              href="https://docs.minaprotocol.com/zkapps"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                <span>DOCS</span>
-                <div>
-                  <Image
-                    src={arrowRightSmall}
-                    alt="Mina Logo"
-                    width={16}
-                    height={16}
-                    priority
-                  />
-                </div>
-              </h2>
-              <p>Explore zkApps, how to build one, and in-depth references</p>
-            </a>
-            <a
-              href="https://docs.minaprotocol.com/zkapps/tutorials/hello-world"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                <span>TUTORIALS</span>
-                <div>
-                  <Image
-                    src={arrowRightSmall}
-                    alt="Mina Logo"
-                    width={16}
-                    height={16}
-                    priority
-                  />
-                </div>
-              </h2>
-              <p>Learn with step-by-step o1js tutorials</p>
-            </a>
-            <a
-              href="https://discord.gg/minaprotocol"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                <span>QUESTIONS</span>
-                <div>
-                  <Image
-                    src={arrowRightSmall}
-                    alt="Mina Logo"
-                    width={16}
-                    height={16}
-                    priority
-                  />
-                </div>
-              </h2>
-              <p>Ask questions on our Discord server</p>
-            </a>
-            <a
-              href="https://docs.minaprotocol.com/zkapps/how-to-deploy-a-zkapp"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                <span>DEPLOY</span>
-                <div>
-                  <Image
-                    src={arrowRightSmall}
-                    alt="Mina Logo"
-                    width={16}
-                    height={16}
-                    priority
-                  />
-                </div>
-              </h2>
-              <p>Deploy a zkApp to Testnet</p>
-            </a>
-          </div>
-        </main>
-      </GradientBG>
-    </>
+                  src={zkwebLogo}
+                  alt="Mina Logo"
+                  width={36}
+                  height={36}
+                  priority
+                />
+              </div>
+            </h2>
+            <p>Correct key was provided</p>
+          </a>
+          <a
+            href=""
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              <span>EXAMPLE 3</span>
+              <div>
+                <Image
+                  src={zkwebLogo}
+                  alt="Mina Logo"
+                  width={36}
+                  height={36}
+                  priority
+                />
+              </div>
+            </h2>
+            <p>Correct key was provided</p>
+          </a>
+          <a
+            href=""
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              <span>EXAMPLE 4</span>
+              <div>
+                <Image
+                  src={zkwebLogo}
+                  alt="Mina Logo"
+                  width={36}
+                  height={36}
+                  priority
+                />
+              </div>
+            </h2>
+            <p>Correct key was provided</p>
+          </a>
+        </div>
+      </main>
+    </GradientBG>
+  </>
   );
 }
