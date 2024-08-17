e909b194868c37fb2acada63003565a18cd09885
Sarah Benson <sarahmbenson96@gmail.com>
2024-08-17 14:29:17 -0500

    supabase

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,49 +1,41 @@
-
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import GradientBG from '../components/GradientBG.js';
 import styles from '../styles/Home.module.css';
-import heroMinaLogo from '../../public/assets/hero-mina-logo.svg';
-import arrowRightSmall from '../../public/assets/arrow-right-small.svg';
 import zkwebLogo from '../../public/assets/zkweb-logo.svg';
 import zkwebBannerImage from '../../public/assets/zkweb-banner-image.svg';
+import { createClient } from '../utils/supabase/client'
+
 
 export default function Home() {
-  const [message, setMessage] = useState('');
+  const supabase = createClient();
   
-  useEffect(() => {
-    (async () => {
-      // const { Mina, PublicKey } = await import('o1js');
-      // const { Add } = await import('../../../contracts/build/src/');
+  const [data, setData] = useState<any>(null);
 
-      // Update this to use the address (public key) for your zkApp account.
-      // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-      // Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
-      const zkAppAddress = '';
-      // This should be removed once the zkAppAddress is updated.
-      if (!zkAppAddress) {
-        console.error(
-          'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
-        );
-      }
-      //const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
-    })();
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: proofs } = await supabase.from("mina-proofs").select();
+      console.log('proofs:', proofs)
+      setData(proofs);
+    };
+    fetchData();
   }, []);
 
-  const deployContract = async () => {
-    console.log('clicked')
-    try {
-      const response = await fetch('/api/deploy', {
-        method: 'POST',
-      });
 
-      const result = await response.json();
-      setMessage(result.data);
-    } catch (error) {
-      console.error('Error deploying contract:', error);
-      setMessage('Deployment failed');
-    }
+  const prove = async () => {
+    console.log('clicked')
+    // try {
+    //   const response = await fetch('/api/deploy', {
+    //     method: 'POST',
+    //   });
+
+    //   const result = await response.json();
+    //   setMessage(result.data);
+    // } catch (error) {
+    //   console.error('Error deploying contract:', error);
+    //   setMessage('Deployment failed');
+    // }
   };
 
   return (
@@ -73,99 +65,37 @@ export default function Home() {
               priority
             />
           </a>
-          {/* <p className={styles.tagline}>
-            built with
-            <code className={styles.code}>o1js</code>
-          </p> */}
+
           <input className="custom-input" placeholder="Enter the secret key..." />
-          <button className="custom-button" onClick={() => deployContract()}>Prove</button>
+          <button className="custom-button" onClick={() => prove()}>Prove</button>
 
         </div>
-        {/* <p className={styles.start}>
-          Get started by editing
-          <code className={styles.code}>src/pages/index.js</code> or <code className={styles.code}>src/pages/index.tsx</code>
-        </p> */}
+
         <div className={styles.grid}>
-          <a
-            href=""
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              <span>EXAMPLE 1</span>
-              <div>
-                <Image
-                  src={zkwebLogo}
-                  alt="Mina Logo"
-                  width={36}
-                  height={36}
-                  priority
-                />
-              </div>
-            </h2>
-            <p>Correct key was provided</p>
-          </a>
-          <a
-            href=""
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              <span>EXAMPLE 2</span>
-              <div>
-              <Image
-                  src={zkwebLogo}
-                  alt="Mina Logo"
-                  width={36}
-                  height={36}
-                  priority
-                />
-              </div>
-            </h2>
-            <p>Correct key was provided</p>
-          </a>
-          <a
-            href=""
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              <span>EXAMPLE 3</span>
-              <div>
-                <Image
-                  src={zkwebLogo}
-                  alt="Mina Logo"
-                  width={36}
-                  height={36}
-                  priority
-                />
-              </div>
-            </h2>
-            <p>Correct key was provided</p>
-          </a>
-          <a
-            href=""
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              <span>EXAMPLE 4</span>
-              <div>
-                <Image
-                  src={zkwebLogo}
-                  alt="Mina Logo"
-                  width={36}
-                  height={36}
-                  priority
-                />
-              </div>
-            </h2>
-            <p>Correct key was provided</p>
-          </a>
+        {data?.map((item: any, index: any) => (
+            <a
+              key={index}
+              href=""
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2>
+                <span>PROOF</span>
+                <div>
+                  <Image
+                    src={zkwebLogo}
+                    alt="Mina Logo"
+                    width={36}
+                    height={36}
+                    priority
+                  />
+                </div>
+              </h2>
+              <p>Correct key was provided</p>
+              <p className="date">{new Date(item.created_at).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+            </a>
+          ))}
         </div>
       </main>
     </GradientBG>
